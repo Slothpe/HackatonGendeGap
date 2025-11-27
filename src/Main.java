4d6fa7babf8bb9d96134158e96f4e2b2d32fecc0
Slothpe <perrulliantonio@outlook.com>
2025-11-27 09:15:47 +0100

    Aggiorno il progetto pt2
    come stai alessio?

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import javafx.application.Application;
 import dashboard.Dashboard;
 
-//Ciao Alessio
+//Ciao Alessio tutto bene ?
 
 public class Main {
     public static void main(String[] args) {
